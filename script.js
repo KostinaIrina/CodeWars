caededc4601b484d4d20db200d16246678d012ae
KostinaIrina <kostinairen92@gmail.com>
2022-02-05 17:39:09 +0300

    +

--- script.js
+++ script.js
@@ -29,3 +29,18 @@ The string has a length greater or equal to one and contains only letters from a
 		i++;
 	}
 	return (count + "/" + a);
+  /*
+Simple, given a string of words, return the length of the shortest word(s).
+String will never be empty and you do not need to account for different data types.
+  */
+  function findShort(s){
+    let res = s.split(' ');
+    let a = Infinity;
+    
+    for (let i = 0; i < res.length; i++) {
+      a = Math.min(res[i].length, a);
+    }
+    return a;
+  }
+  
+  console.log(findShort("Hello my dear friend"));
