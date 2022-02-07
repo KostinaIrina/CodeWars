b57534988447ec3dd4ee6a1ee3d93fab64a882b3
KostinaIrina <kostinairen92@gmail.com>
2022-02-07 16:22:57 +0300

    +

--- script.js
+++ script.js
@@ -182,3 +182,13 @@ function countSmileys(arr) {
   });
   return arr.length == 0 ? 0 : count;
 }
+/*
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+*/
+function solution(str) {
+  if (str.length == 0) {
+    return []
+  };
+  
+  return (str.length % 2 ? str + '_' : str).match(/../g);
+}
