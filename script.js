b370c250be6b8e2c2a17bc1c1ca7503393f5b121
KostinaIrina <kostinairen92@gmail.com>
2022-02-08 19:31:39 +0300

    +

--- script.js
+++ script.js
@@ -192,3 +192,21 @@ function solution(str) {
   
   return (str.length % 2 ? str + '_' : str).match(/../g);
 }
+/*
+You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+n being the length of the string array, if n = 0 or k > n or k <= 0 return "" (return Nothing in Elm).
+*/
+function longestConsec(strarr, k) {
+  if (strarr.length==0 || k>strarr.length || k<=0){
+  return "";
+}else{
+    var list =[];
+    for (var i=0;i<strarr.length-k+1;i++)
+      {
+        list[i]=strarr.slice(i,k+i).join('');
+       }
+
+    var longest = list.reduce(function (a, b) { return a.length >= b.length ? a : b; });
+    return longest;
+}
+}
