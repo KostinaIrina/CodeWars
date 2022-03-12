60292dd11efd0f63deb84b62cd1b896fb2181d57
KostinaIrina <kostinairen92@gmail.com>
2022-03-12 23:26:05 +0300

    +

--- script.js
+++ script.js
@@ -210,3 +210,19 @@ function longestConsec(strarr, k) {
     return longest;
 }
 }
+
+
+/*
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+*/
+
+snail = function(array) {
+  if(array.length === 0) return [];
+  if(array.length === 1) return array[0];
+  let top = array[0].slice(0,-1);
+  let right = array.map(a => a[a.length - 1]);
+  let bottom = array[array.length - 1].slice(0, -1).reverse();
+  let left = array.slice(1, -1).map(b => b[0]).reverse();
+  let inner = array.slice(1, -1).map(c => c.slice(1, -1));
+  return [].concat(top, right, bottom, left, snail(inner));
+}
